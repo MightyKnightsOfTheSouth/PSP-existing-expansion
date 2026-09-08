e62366ef0fbc4ec783a4c241328b960131de5859
MightyKnightsOfTheSouth <phiman645@phimen.us>
2026-09-07 20:10:59 -0400

    Update map layer and attribution details

--- js/map.js
+++ js/map.js
@@ -1,14 +1,15 @@
-        // Geographical center of the US is 39.8283,-98.5795// Create Map
+// Create Map
 const map = L.map('map', {
     center: [37.3,-77.8],
     zoom: 5,
     minZoom: 3
 });
 
-
-// Dark-themed map canvas layer
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    subdomains: 'abcd',
-    maxZoom: 20
+// Dark-themed map layer
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+    maxZoom: 20,
+    attribution:
+        '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> ' +
+        '&copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> ' +
+        '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
 }).addTo(map);
