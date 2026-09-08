4cdfaa1d04fa7805a24a1577149285e33dced7aa
MightyKnightsOfTheSouth <phiman645@phimen.us>
2026-09-07 21:44:42 -0400

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,14 +1,15 @@
-        // Geographical center of the US is 39.8283,-98.5795// Create Map
+// Geographical center of the US is 39.8283, -98.5795
+
+// Create Map
 const map = L.map('map', {
     center: [37.3,-77.8],
     zoom: 5,
     minZoom: 3
 });
 
-
-// Dark-themed map canvas layer
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    subdomains: 'abcd',
-    maxZoom: 20
+// OpenStreetMap raster tiles
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors',
+    maxZoom: 19
 }).addTo(map);
